95fc22303f0b64bef48de77894521332a518f8ba
Mark <mark@>
2021-04-02 22:39:35 +0100

    feat: add wipeTmpDirectory function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+const path = require("path");
 const doesFileExist = require("./does-file-exist");
 const downloadVideoToTmpDirectory = require("./download-video-to-tmp-directory");
 const generateThumbnailsFromVideo = require("./generate-thumbnails-from-video");
@@ -5,6 +7,7 @@ const generateThumbnailsFromVideo = require("./generate-thumbnails-from-video");
 const THUMBNAILS_TO_CREATE = 2;
 
 exports.handler = async (event) => {
+    wipeTmpDirectory();
 	const { videoFileName, triggerBucketName } = extractParams(event);
 	const tmpVideoPath = await downloadVideoToTmpDirectory(triggerBucketName, videoFileName);
 
@@ -19,3 +22,15 @@ const extractParams = event => {
 
 	return { videoFileName, triggerBucketName };
 };
+
+const wipeTmpDirectory = () => {
+    fs.readdir("/tmp/", (err, files) => {
+        if (err) { throw err; }
+
+        for (const file of files) {
+            fs.unlink(path.join("/tmp/", file), err => {
+                if (err) { throw err; }
+            });
+        }
+    });
+}
